6ec7bad864dd6682fe501e6a5794b50331e00bd8
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-03-25 09:47:35 +0000

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let output = 0;
+    if (num1 < 0){
+        return "ERROR";
+    }
+    if (Number.isInteger(num1) == false){
+        return "ERROR";
+    }
+    if (Number.isInteger(num2) == false){
+        return "ERROR";
+    }
+    if (typeof num1 == "string"){
+        return "ERROR";
+    }
+    if (typeof num2 == "string"){
+        return "ERROR";
+    }
+    if (num1 > num2){
+        let num = num1;
+        num1 = num2;
+        num2 = num;
+    }
+    
+    for (let i = num1; i < num2 + 1; i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
