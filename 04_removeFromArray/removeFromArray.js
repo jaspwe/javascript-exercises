f626f8aa11dd3bbebf80a8a1f00acf8a9d8bf182
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-03-25 09:30:23 +0000

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++){
+        if (!remove.includes(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+
 };
 
 // Do not edit below this line
