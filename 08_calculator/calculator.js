bb45c2a3c496e2c341d2ab5c65d147d5e18f7113
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-03-28 15:35:54 +0000

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0){
+    return 0;
+  }
+	return array.reduce((total, current) => {
+    return total + current;
+  })
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => {
+    return total * current;
+  })
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let output = 1;
+  for (i = num; i > 0; i--){
+    output = output * i;
+  }
+  return output;
 };
 
 // Do not edit below this line
