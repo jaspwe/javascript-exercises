35cfb04a1ba22dec06a196aab8333d35f710c17d
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-03-31 12:07:02 +0100

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    for (let i = 0; i < string.length / 2; i++){
+        if (string[i] !== string[string.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
