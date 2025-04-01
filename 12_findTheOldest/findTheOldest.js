823a46e108ba67295b8395f6d536910ce20696af
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-04-01 10:11:54 +0100

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) =>{
+        let deathYear = person.yearOfDeath || 2025;
+        let age = deathYear - person.yearOfBirth;
 
+        let oldestDeathYear = oldest.yearOfDeath || 2025
+        let oldestAge = oldestDeathYear - oldest.yearOfBirth;
+
+        return age > oldestAge ? person : oldest;
+    })
 };
 
 // Do not edit below this line
