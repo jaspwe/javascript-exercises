60a76ed54b2c424d66ccca4488c02f0b1aad4e4f
jaspwe <79276006+jaspwe@users.noreply.github.com>
2025-04-01 09:55:17 +0100

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num)
+    if (num < 0){
+        return "OOPS";
+    }
+    if (num == 0){
+        return 0;
+    }
+    let array = [1, 1];
+    for (let i = 2; i < num; i++)
+        array.push(array[i-2] + array[i-1]);
+    return array[array.length - 1]
 };
 
 // Do not edit below this line
